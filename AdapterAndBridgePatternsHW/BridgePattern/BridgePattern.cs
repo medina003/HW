182d8a393e81783d2bb37eca6de99f7743f2dd5d
medina003 <110483791+medina003@users.noreply.github.com>
2022-12-14 01:53:57 +0400

    Add files via upload

---
+++ AdapterAndBridgePatternsHW/BridgePattern/BridgePattern.cs
@@ -0,0 +1,50 @@
+﻿Teacher MathTeacher = new MathTeacher(new Algebra());
+MathTeacher.Teaches();
+MathTeacher.subject = new Geometry();
+MathTeacher.Teaches();
+public interface ISubject
+{
+    public void Description();
+}
+public class Algebra : ISubject
+{
+   public void Description()
+    {
+        Console.WriteLine("Learning algebra helps to develop your critical thinking skills.");
+    }
+}
+public class Geometry : ISubject
+{
+    public void Description()
+    {
+        Console.WriteLine("Geometry skills include visual skill, descriptive skill, drawing skill, logical skill, applied skill.");
+    }
+}
+public abstract class Teacher
+{
+    public ISubject subject;
+    public ISubject Subject
+    {
+        set { subject = value; }
+    }
+    public Teacher(ISubject sub)
+    {
+        subject = sub;
+    }
+    public virtual void Teaches()
+    {
+        subject.Description();
+    }
+    public abstract void EarnsMoney();
+}
+public class MathTeacher : Teacher
+{
+    public MathTeacher(ISubject sub) : base(sub)
+    {
+    }
+
+    public override void EarnsMoney()
+    {
+        Console.Write("Have 15 working hours a week and earns 700azn");
+    }
+}
