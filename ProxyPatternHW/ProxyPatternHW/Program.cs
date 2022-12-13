a9e89a98a95e2cd2787343941996b73cf8e7866c
medina003 <110483791+medina003@users.noreply.github.com>
2022-12-14 03:06:46 +0400

    Add files via upload

---
+++ ProxyPatternHW/ProxyPatternHW/Program.cs
@@ -0,0 +1,33 @@
+﻿SystemProxy SystemProxy = new(new RealSystem());
+SystemProxy.LogIn("Somebody");
+SystemProxy.LogIn("Admin1");
+public interface ISystem
+{
+    void LogIn(string? user);
+}
+public class RealSystem : ISystem
+{
+    public void LogIn(string? user)
+    {
+        Console.WriteLine($"{user} logged in");
+    }
+}
+public class SystemProxy : ISystem
+{
+    readonly RealSystem realSystem;
+    public SystemProxy(RealSystem realSystem)
+    {
+        this.realSystem = realSystem;
+    }
+    public void LogIn(string? user)
+    {
+        if (!String.IsNullOrEmpty(user))
+        {
+            if (user == "Admin1" || user == "Admin2")
+            {
+                realSystem.LogIn(user);
+            }
+            else Console.WriteLine("This user doesn't have access to the system");
+        }
+    }
+}
