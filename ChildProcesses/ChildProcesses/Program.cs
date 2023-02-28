460e1a98ebca568c684653d0c78e318b880761d2
medina003 <110483791+medina003@users.noreply.github.com>
2023-03-01 02:42:24 +0400

    Add files via upload

---
+++ ChildProcesses/ChildProcesses/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+//Task1
+
+var process = Process.Start("NotePad.exe");
+process.WaitForExit();
+Console.WriteLine(process.ExitCode);
+
+//Task2 
+
+var process2 = Process.Start("NotePad.exe");
+Console.WriteLine("0-Wait\n1-Kill");
+var res = Int32.Parse(Console.ReadLine());
+if(res == 0) { process2.WaitForExit();
+Console.WriteLine(process2.ExitCode);}
+else if(res == 1) { process2.Kill(); }
+
