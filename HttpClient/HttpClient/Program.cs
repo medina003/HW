3849f83c68064495a3af7fe82d52d37177eec98d
Medina <110483791+medina003@users.noreply.github.com>
2023-07-09 21:55:25 +0400

    Add files via upload

---
+++ HttpClient/HttpClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http.Headers;
+var client = new HttpClient();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://currency-exchange.p.rapidapi.com/listquotes"),
+    Headers =
+    {
+        { "X-RapidAPI-Key", "771e57c330msh40a40223bf90afdp1c4c1cjsn40b84fed566b" },
+        { "X-RapidAPI-Host", "currency-exchange.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(body);
+}
